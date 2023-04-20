62311aa5c386667a2d6898d2555f54b5f9296292
Will Minshew <wminshew@gmail.com>
2023-04-19 21:15:53 -0700

    forge update; import w matching case

--- test/UniV3OracleFactory.t.sol
+++ test/UniV3OracleFactory.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-import "splits-tests/base.t.sol";
+import "splits-tests/Base.t.sol";
 
 import {IUniswapV3Factory, UniV3OracleFactory} from "../src/UniV3OracleFactory.sol";
 import {UniV3OracleImpl} from "../src/UniV3OracleImpl.sol";
--- test/UniV3OracleImpl.t.sol
+++ test/UniV3OracleImpl.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-import "splits-tests/base.t.sol";
+import "splits-tests/Base.t.sol";
 
 import {IUniswapV3PoolDerivedState} from "v3-core/interfaces/pool/IUniswapV3PoolDerivedState.sol";
 
