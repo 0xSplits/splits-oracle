c4fd4d8a9a1d9ed89024a4abb782ada455501181
Will Minshew <wminshew@gmail.com>
2023-05-11 22:01:34 -0700

    refactor tests

---
+++ test/UniV3OracleImpl/UniV3OracleImpl.t.sol
@@ -0,0 +1,310 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import "splits-tests/Base.t.sol";
+
+import
+/* Initialized_PausableImplBase, */
+{
+    Initialized_PausableImplTest,
+    /*     Uninitialized_PausableImplBase, */
+    Uninitialized_PausableImplTest
+} from "splits-tests/PausableImpl/PausableImpl.t.sol";
+import {IUniswapV3Factory} from "v3-core/interfaces/IUniswapV3Factory.sol";
+import {IUniswapV3PoolDerivedState} from "v3-core/interfaces/pool/IUniswapV3PoolDerivedState.sol";
+import {QuotePair, QuoteParams} from "splits-utils/LibQuotes.sol";
+
+import {
+    Initialized_UniV3OracleImplBase,
+    Paused_Initialized_UniV3OracleImplBase,
+    Uninitialized_UniV3OracleImplBase,
+    Unpaused_Initialized_UniV3OracleImplBase
+} from "./UniV3OracleImplBase.t.sol";
+
+import {UniV3OracleFactory} from "../../src/UniV3OracleFactory.sol";
+import {IOracle} from "../../src/interfaces/IOracle.sol";
+import {OracleParams} from "../../src/peripherals/OracleParams.sol";
+import {UniV3OracleImpl} from "../../src/UniV3OracleImpl.sol";
+
+contract Unintialized_UniV3OracleImplTest is Uninitialized_PausableImplTest, Uninitialized_UniV3OracleImplBase {
+    function setUp() public virtual override(Uninitialized_PausableImplTest, Uninitialized_UniV3OracleImplBase) {
+        Uninitialized_UniV3OracleImplBase.setUp();
+    }
+
+    function _initialize()
+        internal
+        virtual
+        override(Uninitialized_PausableImplTest, Uninitialized_UniV3OracleImplBase)
+    {
+        Uninitialized_UniV3OracleImplBase._initialize();
+    }
+
+    /// -----------------------------------------------------------------------
+    /// initializer
+    /// -----------------------------------------------------------------------
+
+    function testFork_revertWhen_callerNotFactory_initializer() public callerNotFactory($notFactory) {
+        vm.expectRevert(Unauthorized.selector);
+        $oracle.initializer(_initParams());
+    }
+
+    function testForkFuzz_revertWhen_callerNotFactory_initializer(
+        address caller_,
+        UniV3OracleImpl.InitParams calldata params_
+    ) public callerNotFactory(caller_) {
+        _setUpUniV3OracleParams(params_);
+
+        testFork_revertWhen_callerNotFactory_initializer();
+    }
+
+    function testFork_initializer_setsDefaultPeriod() public callerFactory {
+        $oracle.initializer(_initParams());
+        assertEq($oracle.defaultPeriod(), $defaultPeriod);
+    }
+
+    function testForkFuzz_initializer_setsDefaultPeriod(UniV3OracleImpl.InitParams calldata params_)
+        public
+        callerFactory
+    {
+        _setUpUniV3OracleParams(params_);
+        testFork_initializer_setsDefaultPeriod();
+    }
+
+    function testFork_initializer_setsPairDetails() public callerFactory {
+        UniV3OracleImpl.InitParams memory initParams = _initParams();
+        $oracle.initializer(initParams);
+
+        uint256 length = initParams.pairDetails.length;
+        QuotePair[] memory initQuotePairs = new QuotePair[](length);
+        UniV3OracleImpl.PairDetail[] memory initPairDetails = new UniV3OracleImpl.PairDetail[](length);
+        for (uint256 i; i < length; i++) {
+            initQuotePairs[i] = initParams.pairDetails[i].quotePair;
+            initPairDetails[i] = initParams.pairDetails[i].pairDetail;
+        }
+        assertEq($oracle.getPairDetails(initQuotePairs), initPairDetails);
+    }
+
+    function testForkFuzz_initializer_setsPairDetails(UniV3OracleImpl.InitParams calldata params_)
+        public
+        callerFactory
+    {
+        _setUpUniV3OracleParams(params_);
+        testFork_initializer_setsPairDetails();
+    }
+}
+
+contract Initialized_UniV3OracleImplTest is Initialized_PausableImplTest, Initialized_UniV3OracleImplBase {
+    function setUp() public virtual override(Initialized_PausableImplTest, Initialized_UniV3OracleImplBase) {
+        Initialized_UniV3OracleImplBase.setUp();
+    }
+
+    function _initialize() internal virtual override(Initialized_PausableImplTest, Initialized_UniV3OracleImplBase) {
+        Initialized_UniV3OracleImplBase._initialize();
+    }
+
+    /// -----------------------------------------------------------------------
+    /// setDefaultPeriod
+    /// -----------------------------------------------------------------------
+
+    function testFork_revertWhen_callerNotOwner_setDefaultPeriod() public callerNotOwner($notOwner) {
+        vm.expectRevert(Unauthorized.selector);
+        $oracle.setDefaultPeriod($nextDefaultPeriod);
+    }
+
+    function testForkFuzz_revertWhen_callerNotOwner_setDefaultPeriod(address notOwner_, uint32 nextDefaultPeriod_)
+        public
+        callerNotOwner(notOwner_)
+    {
+        vm.expectRevert(Unauthorized.selector);
+        $oracle.setDefaultPeriod(nextDefaultPeriod_);
+    }
+
+    function testFork_setDefaultPeriod_setsDefaultPeriod() public callerOwner {
+        $oracle.setDefaultPeriod($nextDefaultPeriod);
+        assertEq($oracle.defaultPeriod(), $nextDefaultPeriod);
+    }
+
+    function testForkFuzz_setDefaultPeriod_setsDefaultPeriod(uint32 nextDefaultPeriod_) public callerOwner {
+        $oracle.setDefaultPeriod(nextDefaultPeriod_);
+        assertEq($oracle.defaultPeriod(), nextDefaultPeriod_);
+    }
+
+    function testFork_setDefaultPeriod_emitsSetDefaultPeriod() public callerOwner {
+        vm.expectEmit();
+        emit SetDefaultPeriod($nextDefaultPeriod);
+        $oracle.setDefaultPeriod($nextDefaultPeriod);
+    }
+
+    function testForkFuzz_setDefaultPeriod_emitsSetDefaultPeriod(uint32 nextDefaultPeriod_) public callerOwner {
+        vm.expectEmit();
+        emit SetDefaultPeriod(nextDefaultPeriod_);
+        $oracle.setDefaultPeriod(nextDefaultPeriod_);
+    }
+
+    /// -----------------------------------------------------------------------
+    /// setPairDetails
+    /// -----------------------------------------------------------------------
+
+    function testFork_revertWhen_callerNotOwner_setPairDetails() public callerNotOwner($notOwner) {
+        vm.expectRevert(Unauthorized.selector);
+        $oracle.setPairDetails($nextPairDetails);
+    }
+
+    function testForkFuzz_revertWhen_callerNotOwner_setPairDetails(
+        address notOwner_,
+        UniV3OracleImpl.SetPairDetailParams[] memory nextSetPairDetails_
+    ) public callerNotOwner(notOwner_) {
+        vm.expectRevert(Unauthorized.selector);
+        $oracle.setPairDetails(nextSetPairDetails_);
+    }
+
+    function testFork_setPairDetails_setsPairDetails() public callerOwner {
+        $oracle.setPairDetails($nextPairDetails);
+
+        uint256 length = $nextPairDetails.length;
+        QuotePair[] memory quotePairs = new QuotePair[](length);
+        UniV3OracleImpl.PairDetail[] memory nextPairDetails = new UniV3OracleImpl.PairDetail[](length);
+        for (uint256 i; i < length; i++) {
+            quotePairs[i] = $nextPairDetails[i].quotePair;
+            nextPairDetails[i] = $nextPairDetails[i].pairDetail;
+        }
+        assertEq($oracle.getPairDetails(quotePairs), nextPairDetails);
+    }
+
+    // TODO: upgrade to test array; need to prune converted duplicates or limit size to 2 or something
+    function testForkFuzz_setPairDetails_setsPairDetails(UniV3OracleImpl.SetPairDetailParams memory nextSetPairDetails_)
+        public
+        callerOwner
+    {
+        uint256 length = 1;
+        UniV3OracleImpl.SetPairDetailParams[] memory nextSetPairDetails =
+            new UniV3OracleImpl.SetPairDetailParams[](length);
+        nextSetPairDetails[0] = nextSetPairDetails_;
+
+        $oracle.setPairDetails(nextSetPairDetails);
+
+        QuotePair[] memory quotePairs = new QuotePair[](length);
+        UniV3OracleImpl.PairDetail[] memory newPairDetails = new UniV3OracleImpl.PairDetail[](length);
+        for (uint256 i; i < length; i++) {
+            quotePairs[i] = nextSetPairDetails[i].quotePair;
+            newPairDetails[i] = nextSetPairDetails[i].pairDetail;
+        }
+        assertEq($oracle.getPairDetails(quotePairs), newPairDetails);
+    }
+
+    function testFork_setPairDetails_emitsSetPairDetails() public callerOwner {
+        vm.expectEmit();
+        emit SetPairDetails($nextPairDetails);
+        $oracle.setPairDetails($nextPairDetails);
+    }
+
+    function testForkFuzz_setPairDetails_emitsSetPairDetails(
+        UniV3OracleImpl.SetPairDetailParams[] memory nextSetPairDetails_
+    ) public callerOwner {
+        vm.expectEmit();
+        emit SetPairDetails(nextSetPairDetails_);
+        $oracle.setPairDetails(nextSetPairDetails_);
+    }
+}
+
+contract Paused_Initialized_UniV3OracleImplTest is
+    Initialized_UniV3OracleImplTest,
+    Paused_Initialized_UniV3OracleImplBase
+{
+    function setUp() public virtual override(Paused_Initialized_UniV3OracleImplBase, Initialized_UniV3OracleImplTest) {
+        Paused_Initialized_UniV3OracleImplBase.setUp();
+    }
+
+    function _initialize()
+        internal
+        virtual
+        override(Initialized_UniV3OracleImplBase, Initialized_UniV3OracleImplTest)
+    {
+        Initialized_UniV3OracleImplBase._initialize();
+    }
+
+    /// -----------------------------------------------------------------------
+    /// getQuoteAmounts
+    /// -----------------------------------------------------------------------
+
+    function testFork_revertWhen_paused_getQuoteAmounts() public paused {
+        vm.expectRevert(Paused.selector);
+        $oracle.getQuoteAmounts($usdcQuoteParams);
+    }
+
+    function testForkFuzz_revertWhen_paused_getQuoteAmounts(address caller_, QuoteParams[] calldata quoteParams_)
+        public
+        paused
+    {
+        changePrank(caller_);
+        vm.expectRevert(Paused.selector);
+        $oracle.getQuoteAmounts(quoteParams_);
+    }
+}
+
+contract Unpaused_Initialized_UniV3OracleImplTest is
+    Initialized_UniV3OracleImplTest,
+    Unpaused_Initialized_UniV3OracleImplBase
+{
+    using TokenUtils for address;
+
+    function setUp() public virtual override(Initialized_UniV3OracleImplBase, Initialized_UniV3OracleImplTest) {
+        Initialized_UniV3OracleImplBase.setUp();
+    }
+
+    function _initialize()
+        internal
+        virtual
+        override(Initialized_UniV3OracleImplBase, Initialized_UniV3OracleImplTest)
+    {
+        Initialized_UniV3OracleImplBase._initialize();
+    }
+
+    /// -----------------------------------------------------------------------
+    /// getQuoteAmounts
+    /// -----------------------------------------------------------------------
+
+    function testFail_getQuoteAmounts_ifConvertedPairEqual_doesntCallUniGetPool() public unpaused {
+        vm.expectCall({callee: UNISWAP_V3_FACTORY, data: ""});
+        $oracle.getQuoteAmounts($wethQuoteParams);
+    }
+
+    function testFork_getQuoteAmounts_ifConvertedPairEqual_returnsBaseAmount() public unpaused {
+        uint256[] memory quoteAmounts = $oracle.getQuoteAmounts($wethQuoteParams);
+        assertEq(quoteAmounts[0], $wethQuoteParams[0].baseAmount);
+    }
+
+    function testFork_revertsWhen_PoolNotSet_getQuoteAmounts() public unpaused {
+        vm.expectRevert(InvalidPair_PoolNotSet.selector);
+        $oracle.getQuoteAmounts($mockERC20QuoteParams);
+    }
+
+    function testFork_getQuoteAmounts_callsPoolObserveWithDefaultPeriod() public unpaused {
+        address pool = IUniswapV3Factory(UNISWAP_V3_FACTORY).getPool(USDC, WETH9, 5_00);
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = $defaultPeriod;
+        secondsAgos[1] = 0;
+
+        vm.expectCall({callee: pool, data: abi.encodeCall(IUniswapV3PoolDerivedState.observe, (secondsAgos))});
+        $oracle.getQuoteAmounts($usdcQuoteParams);
+    }
+
+    function testFork_getQuoteAmounts_callsPoolObserveWithDetailsPeriod() public unpaused {
+        address pool = IUniswapV3Factory(UNISWAP_V3_FACTORY).getPool(DAI, WETH9, 5_00);
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = 60 minutes;
+        secondsAgos[1] = 0;
+
+        vm.expectCall({callee: pool, data: abi.encodeCall(IUniswapV3PoolDerivedState.observe, (secondsAgos))});
+        $oracle.getQuoteAmounts($daiQuoteParams);
+    }
+
+    function testFork_getQuoteAmounts_returnsApproximateAmount() public unpaused {
+        // etherscan eth price on block
+        // $1,909.49 / ETH
+        // 1000 usdc -> ~0.52 eth
+        uint256[] memory amounts = $oracle.getQuoteAmounts($usdcQuoteParams);
+        assertTrue(amounts[0] > 0.515 ether);
+        assertTrue(amounts[0] < 0.525 ether);
+    }
+}
---
+++ test/UniV3OracleImpl/UniV3OracleImplBase.t.sol
@@ -0,0 +1,267 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import "splits-tests/Base.t.sol";
+
+import {
+    Initialized_PausableImplBase,
+    PausableImplHarness,
+    Uninitialized_PausableImplBase
+} from "splits-tests/PausableImpl/PausableImplBase.t.sol";
+import {IUniswapV3Factory} from "v3-core/interfaces/IUniswapV3Factory.sol";
+import {OwnableImplHarness} from "splits-tests/OwnableImpl/OwnableImplBase.t.sol";
+import {QuotePair, QuoteParams} from "splits-utils/LibQuotes.sol";
+
+import {UniV3OracleFactory} from "../../src/UniV3OracleFactory.sol";
+import {UniV3OracleImpl} from "../../src/UniV3OracleImpl.sol";
+
+// State tree
+//  Uninitialized
+//  Initialized
+//   Paused
+//   Unpaused
+
+abstract contract Uninitialized_UniV3OracleImplBase is Uninitialized_PausableImplBase {
+    error InvalidPair_PoolNotSet();
+
+    event SetDefaultPeriod(uint32 defaultPeriod);
+    event SetPairDetails(UniV3OracleImpl.SetPairDetailParams[] params);
+
+    UniV3OracleFactory $oracleFactory;
+    UniV3OracleImpl $oracle;
+
+    uint32 $defaultPeriod;
+    uint32 $nextDefaultPeriod;
+    address $notFactory;
+
+    QuotePair $wethETH;
+    QuotePair $usdcETH;
+    QuotePair $daiETH;
+    QuotePair $mockERC20ETH;
+
+    UniV3OracleImpl.SetPairDetailParams[] $pairDetails;
+    UniV3OracleImpl.SetPairDetailParams[] $nextPairDetails;
+
+    QuoteParams[] $wethQuoteParams;
+    QuoteParams[] $usdcQuoteParams;
+    QuoteParams[] $daiQuoteParams;
+    QuoteParams[] $mockERC20QuoteParams;
+
+    function setUp() public virtual override {
+        Uninitialized_PausableImplBase.setUp();
+
+        vm.createSelectFork(vm.rpcUrl("mainnet"), BLOCK_NUMBER);
+
+        $oracleFactory = new UniV3OracleFactory({
+            weth9_: WETH9
+        });
+
+        $wethETH = QuotePair({base: WETH9, quote: ETH_ADDRESS});
+        $usdcETH = QuotePair({base: USDC, quote: ETH_ADDRESS});
+        $daiETH = QuotePair({base: DAI, quote: ETH_ADDRESS});
+        $mockERC20ETH = QuotePair({base: address(mockERC20), quote: ETH_ADDRESS});
+
+        $pairDetails.push(
+            UniV3OracleImpl.SetPairDetailParams({
+                quotePair: $usdcETH,
+                pairDetail: UniV3OracleImpl.PairDetail({
+                    pool: IUniswapV3Factory(UNISWAP_V3_FACTORY).getPool(USDC, WETH9, 5_00),
+                    period: 0 // no override
+                })
+            })
+        );
+        $pairDetails.push(
+            UniV3OracleImpl.SetPairDetailParams({
+                quotePair: $daiETH,
+                pairDetail: UniV3OracleImpl.PairDetail({
+                    pool: IUniswapV3Factory(UNISWAP_V3_FACTORY).getPool(DAI, WETH9, 5_00),
+                    period: 60 minutes
+                })
+            })
+        );
+
+        $nextPairDetails.push(
+            UniV3OracleImpl.SetPairDetailParams({
+                quotePair: $usdcETH,
+                pairDetail: UniV3OracleImpl.PairDetail({
+                    pool: IUniswapV3Factory(UNISWAP_V3_FACTORY).getPool(USDC, WETH9, 30_00),
+                    period: 0 // no override
+                })
+            })
+        );
+
+        $wethQuoteParams.push(QuoteParams({quotePair: $wethETH, baseAmount: 1 ether, data: ""}));
+
+        $usdcQuoteParams.push(
+            QuoteParams({
+                quotePair: $usdcETH,
+                baseAmount: 10 ** 9, // $1,000 usc ~= 0.5 eth
+                data: ""
+            })
+        );
+
+        $daiQuoteParams.push(
+            QuoteParams({
+                quotePair: $daiETH,
+                baseAmount: 10 ** 21, // $1,000 dai ~= 0.5 eth
+                data: ""
+            })
+        );
+
+        $mockERC20QuoteParams.push(
+            QuoteParams({
+                quotePair: $mockERC20ETH,
+                baseAmount: uint128((10 ** 3) * (10 ** MOCK_ERC20_DECIMALS)),
+                data: ""
+            })
+        );
+
+        _setUpUniV3OracleImplState({
+            oracle_: address($oracleFactory.uniV3OracleImpl()),
+            owner_: users.alice,
+            paused_: false,
+            defaultPeriod_: 30 minutes,
+            nextDefaultPeriod_: 60 minutes,
+            notFactory_: users.eve,
+            pairDetails_: $pairDetails,
+            nextPairDetails_: $nextPairDetails,
+            wethQuoteParams_: $wethQuoteParams,
+            usdcQuoteParams_: $usdcQuoteParams,
+            daiQuoteParams_: $daiQuoteParams,
+            mockERC20QuoteParams_: $mockERC20QuoteParams
+        });
+    }
+
+    function _setUpUniV3OracleImplState(
+        address oracle_,
+        address owner_,
+        bool paused_,
+        uint32 defaultPeriod_,
+        uint32 nextDefaultPeriod_,
+        address notFactory_,
+        UniV3OracleImpl.SetPairDetailParams[] memory pairDetails_,
+        UniV3OracleImpl.SetPairDetailParams[] memory nextPairDetails_,
+        QuoteParams[] memory wethQuoteParams_,
+        QuoteParams[] memory usdcQuoteParams_,
+        QuoteParams[] memory daiQuoteParams_,
+        QuoteParams[] memory mockERC20QuoteParams_
+    ) internal virtual {
+        _setUpPausableImplState({pausable_: oracle_, paused_: paused_});
+
+        $oracle = UniV3OracleImpl(oracle_);
+        $owner = owner_;
+        $paused = paused_;
+        $defaultPeriod = defaultPeriod_;
+        $nextDefaultPeriod = nextDefaultPeriod_;
+        $notFactory = notFactory_;
+
+        delete $pairDetails;
+        for (uint256 i = 0; i < pairDetails_.length; i++) {
+            $pairDetails.push(pairDetails_[i]);
+        }
+
+        delete $nextPairDetails;
+        for (uint256 i = 0; i < nextPairDetails_.length; i++) {
+            $nextPairDetails.push(nextPairDetails_[i]);
+        }
+
+        delete $wethQuoteParams;
+        for (uint256 i = 0; i < wethQuoteParams_.length; i++) {
+            $wethQuoteParams.push(wethQuoteParams_[i]);
+        }
+
+        delete $usdcQuoteParams;
+        for (uint256 i = 0; i < usdcQuoteParams_.length; i++) {
+            $usdcQuoteParams.push(usdcQuoteParams_[i]);
+        }
+
+        delete $daiQuoteParams;
+        for (uint256 i = 0; i < daiQuoteParams_.length; i++) {
+            $daiQuoteParams.push(daiQuoteParams_[i]);
+        }
+
+        delete $mockERC20QuoteParams;
+        for (uint256 i = 0; i < mockERC20QuoteParams_.length; i++) {
+            $mockERC20QuoteParams.push(mockERC20QuoteParams_[i]);
+        }
+    }
+
+    function _setUpUniV3OracleParams(UniV3OracleImpl.InitParams memory params_) internal virtual {
+        $owner = params_.owner;
+        $paused = params_.paused;
+        $defaultPeriod = params_.defaultPeriod;
+
+        delete $pairDetails;
+        for (uint256 i = 0; i < params_.pairDetails.length; i++) {
+            $pairDetails.push(params_.pairDetails[i]);
+        }
+    }
+
+    function _initParams() internal view returns (UniV3OracleImpl.InitParams memory) {
+        return UniV3OracleImpl.InitParams({
+            owner: $owner,
+            paused: $paused,
+            defaultPeriod: $defaultPeriod,
+            pairDetails: $pairDetails
+        });
+    }
+
+    function _initialize() internal virtual override {
+        $oracle = $oracleFactory.createUniV3Oracle(_initParams());
+        $ownable = OwnableImplHarness(address($oracle));
+        $pausable = PausableImplHarness(address($oracle));
+    }
+
+    /// -----------------------------------------------------------------------
+    /// modifiers
+    /// -----------------------------------------------------------------------
+
+    modifier callerNotFactory(address notFactory_) {
+        vm.assume(notFactory_ != address($oracleFactory));
+        $notFactory = notFactory_;
+        changePrank(notFactory_);
+        _;
+    }
+
+    modifier callerFactory() {
+        changePrank(address($oracleFactory));
+        _;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// internal
+    /// -----------------------------------------------------------------------
+
+    function assertEq(UniV3OracleImpl.PairDetail[] memory a, UniV3OracleImpl.PairDetail[] memory b) internal {
+        assertEq(a.length, b.length);
+        for (uint256 i; i < a.length; i++) {
+            assertEq(a[i], b[i]);
+        }
+    }
+
+    function assertEq(UniV3OracleImpl.PairDetail memory a, UniV3OracleImpl.PairDetail memory b) internal {
+        assertEq(a.pool, b.pool);
+        assertEq(a.period, b.period);
+    }
+}
+
+abstract contract Initialized_UniV3OracleImplBase is Uninitialized_UniV3OracleImplBase, Initialized_PausableImplBase {
+    function setUp() public virtual override(Uninitialized_UniV3OracleImplBase, Initialized_PausableImplBase) {
+        Uninitialized_UniV3OracleImplBase.setUp();
+        _initialize();
+    }
+
+    function _initialize() internal virtual override(Uninitialized_UniV3OracleImplBase, Initialized_PausableImplBase) {
+        Uninitialized_UniV3OracleImplBase._initialize();
+    }
+}
+
+abstract contract Paused_Initialized_UniV3OracleImplBase is Initialized_UniV3OracleImplBase {
+    function setUp() public virtual override {
+        Uninitialized_UniV3OracleImplBase.setUp();
+        $paused = true;
+        _initialize();
+    }
+}
+
+abstract contract Unpaused_Initialized_UniV3OracleImplBase is Initialized_UniV3OracleImplBase {}
