d186d74b4d1e5f51e30aff07198426eb3e4c6e77
Rooh Afza <coldroohafza@gmail.com>
2023-12-11 18:45:24 -0800

    deploy: default uniswap oracle to base

--- script/02_CreateDefaultOracle.coinbase.s.sol
+++ script/02_CreateDefaultOracle.coinbase.s.sol
@@ -10,7 +10,8 @@ contract CreateDefaultOracleCoinbaseScript is CreateDefaultOracleBaseScript {
     using stdJson for string;
 
     address constant WETH9 = 0x4200000000000000000000000000000000000006;
-    address constant USDC = 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA;
+    address constant USDbC = 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA;
+    address constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
 
     function setUp() public {
         $owner = address(0x0);
@@ -19,6 +20,10 @@ contract CreateDefaultOracleCoinbaseScript is CreateDefaultOracleBaseScript {
 
         $pairDetails.push(
             PairDetail({tokenA: WETH9, tokenB: USDC, poolFee: POINT_ZERO_FIVE_PERCENT, period: uint32(0)})
+        );
+
+        $pairDetails.push(
+            PairDetail({tokenA: WETH9, tokenB: USDbC, poolFee: POINT_ZERO_FIVE_PERCENT, period: uint32(0)})
         );
     }
 }
